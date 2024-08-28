c744e7942ca0ac16f7611b6af2dfa439bed35cee
Duvan <duvanriano2626@gmail.com>
2024-08-27 21:23:28 -0500

    Creacion de nuestro servicio para la autenticacion

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
         component: LoginPageComponent,
       },
       {
-        path:'resgister',
+        path:'register',
         component: RegisterPageComponent,
       },
       {
