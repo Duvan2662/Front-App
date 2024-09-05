f7eedb12c4466e13bac35e06beddc0f493f8b36c
Duvan <duvanriano2626@gmail.com>
2024-09-05 15:55:31 -0500

    Creacion del guard para que el usuario no pueda ir al login si ya esta autenticado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { isAuthenticatedGuard } from './auth/guards/is-authenticated.guard';
+import { isNotAuthenticatedGuard } from './auth/guards/is-not-authenticated.guard';
 
 
 const routes: Routes = [
@@ -8,11 +9,12 @@ const routes: Routes = [
   {
     path:'auth',
     //guards
+    canActivate:[isNotAuthenticatedGuard,],
     loadChildren: () => import('./auth/auth.module').then(m=>m.AuthModule)
   },
   {
     path:'dashboard',
-    canActivate:[isAuthenticatedGuard],
+    canActivate:[isAuthenticatedGuard,],
     loadChildren:() => import('./dashboard/dashboard.module').then(m=>m.DashboardModule)
   },
   {
