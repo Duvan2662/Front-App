fa3b6197b50da919ce26f996ccbe1eb9ac3de3fb
Duvan <duvanriano2626@gmail.com>
2024-09-05 13:26:47 -0500

    Autenticacion de nuestro login con nuestro backend se habilitaron los cors en el backen atento con eso

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { AuthStatus, User } from '../interfaces';
+import { map, Observable,tap } from 'rxjs';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +16,37 @@ export class AuthService {
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
 
-
-
-  public login = (email:string, password:string): Observable<boolean> => {
-
-    return of(true)
-  }
-
+  //!Al mundo exterior
+  public currentUser = computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
 
 
   constructor() { }
+
+
+
+
+  public login = (email:string, password:string): Observable<boolean> => {
+    const url = `${this.baseUrl}/auth/login`;
+    const body = {
+      email: email,
+      password: password
+    }
+
+    return this.http.post<LoginResponse>(url,body)
+      .pipe(
+        tap(({user, token}) =>{
+          this._currentUser.set(user);
+          this._authStatus.set(AuthStatus.authenticated);
+          localStorage.setItem('token',token)
+          console.log({user,token});
+        }),
+        
+        map(() => true)
+
+        // Todo: errores
+      );
+
+
+  }
 }
